fbc30b60d66b6c05acd598d0432967ad9c9d23f2
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-05-09 00:29:47 +0900

    Add configures

--- config/environments.rb
+++ config/environments.rb
@@ -1,3 +1,8 @@
+require 'sinatra'
+
 ROOT_DIR = File.expand_path('..', __dir__)
 CONTROLLERS_DIR = File.expand_path('../controllers', __dir__)
-VIEWS_DIR = File.expand_path('../views', __dir__)
+
+configure do
+  set :root, ROOT_DIR
+end
