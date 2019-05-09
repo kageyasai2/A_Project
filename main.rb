6b1d157671152e0178dc6a97f9ad586131132f67
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-05-09 14:36:05 +0900

    Add to require models

--- main.rb
+++ main.rb
@@ -6,6 +6,10 @@ Dir[CONTROLLERS_DIR + '/*.rb'].each do |controller|
   require controller
 end
 
+Dir[MODELS_DIR + '/*.rb'].each do |model|
+  require model
+end
+
 class Main < Sinatra::Base
   ROUTES = {
     '/' => Index,
