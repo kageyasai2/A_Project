d9676173dd53d07b793d58e0fa3b70d6ef1d67b3
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-05-09 14:36:32 +0900

    Add spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../config/environments'
+require File.join(ROOT_DIR, 'main.rb')
+
+require 'rspec'
+require 'rack/test'
+
+set :environment, :test
+set :run, false
+set :raise_errors, true
+set :logging, false
+
+RSpec.configure do |conf|
+  conf.include Rack::Test::Methods
+end
+
+def app
+  Sinatra::Application
+end
