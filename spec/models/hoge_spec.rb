07230347ef54127df6791ef823ca35d8eb71eda2
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-05-12 23:46:13 +0900

    Add active record test to example

--- spec/models/hoge_spec.rb
+++ spec/models/hoge_spec.rb
@@ -4,4 +4,10 @@ describe Hoge do
   it 'should print String' do
     expect(Hoge.new.puts).to be_instance_of String
   end
+
+  it 'should save instance' do
+    Hoge.create(name: 'hey')
+
+    expect(Hoge.all.length).to be 1
+  end
 end
