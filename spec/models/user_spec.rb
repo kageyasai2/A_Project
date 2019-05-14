cd0b36753a37e066b0ef259d3e481136d077f011
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-05-14 12:05:37 +0900

    Add email validates test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,25 @@
 require 'spec_helper'
 
 describe User do
-  it 'is valid' do
-    @user = User.create(
+  before do
+    @user = User.new(
       name: 'NAKKA',
       email: 'nakka@example.com',
       password: 'password',
       password_confirmation: 'password'
     )
 
+    # テストするために同じemailのUserを全て削除する
+    User.where(email: @user.email).destroy_all
+  end
+
+  it 'is valid' do
     expect(@user).to be_valid
+  end
+
+  it 'is invalid email' do
+    @user.email = 'example.com'
+    expect(@user).to_not be_valid
   end
 end
 
