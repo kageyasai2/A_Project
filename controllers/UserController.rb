0876d4da12cb1acbfe4c7f0d6aa950d7dfe58c43
kageyasai2 <44107494+kageyasai2@users.noreply.github.com>
2019-05-15 11:46:51 +0900

    [change]file name & [fix] 12 ~ 17 line

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -3,13 +3,18 @@ require_relative 'base'
 
 require './models/user.rb'
 
-class User_controller < Base
+class UserController < Base
   get '/' do
     erb :'user/signup'
   end
 
   post '/new' do
-    user = User.new({name:params[:name],email:params[:email],password:params[:password],password_confirmation:params[:password_confirmation]})
+    user = User.new({
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    })
     if user.save
       erb :'user/login'
     else
