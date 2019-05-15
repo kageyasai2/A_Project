3e5153eb37e636965dd9705c2d0cd7ac8581f93a
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-05-15 14:24:16 +0900

    Update logout spec

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
-describe Sessions do
-  it 'is valid' do
-    get '/auth/logout'
-    expect(last_response.status).to eq 302
+describe 'Sessions controller', type: :request do
+  describe 'GET to /auth/logout' do
+    it 'returns 302 status' do
+      get '/auth/logout'
+      expect(last_response.status).to eq 302
+    end
   end
 end
 
