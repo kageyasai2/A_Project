0d5980dcb5197a065764f2c28fdf7a9fda509950
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-05-16 14:35:58 +0900

    [add]food register

--- models/userfood.rb
+++ models/userfood.rb
@@ -2,6 +2,7 @@ require 'active_record'
 
 class Userfood < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
+  validates :limit_date, presence: true
   validates :gram, presence: true
   belongs_to :user
 end
