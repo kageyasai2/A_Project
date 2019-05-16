73af0ec056d0925ca5eeea2a0415e8dafe73494d
kageyasai2 <44107494+kageyasai2@users.noreply.github.com>
2019-05-16 14:43:15 +0900

    [change]validation
    食材名だけ入力必須

--- models/userfood.rb
+++ models/userfood.rb
@@ -2,7 +2,5 @@ require 'active_record'
 
 class Userfood < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
-  validates :limit_date, presence: true
-  validates :gram, presence: true
   belongs_to :user
 end
