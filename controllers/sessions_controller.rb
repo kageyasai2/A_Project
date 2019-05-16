15123a4468b5b370c61c229fd4d941efaf79df98
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-05-16 10:46:55 +0900

    Add login controller

--- controllers/sessions_controller.rb
+++ controllers/sessions_controller.rb
@@ -6,6 +6,23 @@ class SessionsController < Base
     erb :'sessions/login'
   end
 
+  post '/login' do
+    if params[:email] && params[:password]
+      erb :'sessions/login'
+    end
+
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+    else
+      @email = params[:email]
+      @password = params[:password]
+
+      erb :'sessions/login'
+    end
+  end
+
   get '/logout' do
     session[:user_id] = nil
 
