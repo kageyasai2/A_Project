71cded257cb6a83c29da42fac2eb41ec333a458c
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-05-16 10:22:29 +0900

    [add]Userfoodsテーブルにカラム追加

--- db/migrate/20190516005738_create_userfoods.rb
+++ db/migrate/20190516005738_create_userfoods.rb
@@ -1,4 +1,12 @@
 class CreateUserfoods < ActiveRecord::Migration[5.2]
   def change
+    create_table :userfoods do |t|
+      t.string  :name
+      t.date    :limit_date
+      t.integer :user_id
+      t.integer :gram
+
+      t.timestamps
+    end
   end
 end
