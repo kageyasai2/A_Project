eac2015d250a0695cb1b2069a213ab631669bdc3
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-05-16 09:58:31 +0900

    Add controller that returns login view

--- controllers/sessions_controller.rb
+++ controllers/sessions_controller.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 require_relative 'base'
 
 class SessionsController < Base
+  get '/login' do
+    erb :'sessions/login'
+  end
+
   get '/logout' do
     session[:user_id] = nil
 
