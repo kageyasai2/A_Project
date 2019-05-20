9ea7f4071ff68bd7d7fe42811bb8cab665f96293
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-05-20 19:15:13 +0900

    Add user_foods variable to home

--- controllers/index.rb
+++ controllers/index.rb
@@ -7,6 +7,11 @@ class IndexController < Base
   end
 
   get '/home' do
+    if session[:user_id]
+      @user_foods = UserFood.where(user_id: session[:user_id])
+    end
+    @user_foods ||= []
+
     erb :home
   end
 end
