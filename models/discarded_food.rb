5152906af3405ce444033d0ceec6c31dfdee1a87
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-05-29 15:07:25 +0900

    Add scopes of discarded food

--- models/discarded_food.rb
+++ models/discarded_food.rb
@@ -3,4 +3,10 @@ require 'active_record'
 class DiscardedFood < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   belongs_to :user
+
+  scope :where_current_year, -> { where(created_at: Time.current.all_year) }
+  scope :where_current_month, -> { where(created_at: Time.current.all_month) }
+  scope :group_by_month, -> { group('MONTH(created_at)') }
+  scope :group_by_date, -> { group('DATE(created_at)') }
+  scope :sum_calorie, -> { sum(:calorie) }
 end
