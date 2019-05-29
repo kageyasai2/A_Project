bf18acff23e530101f8ed9269d3c87cc7cb9d571
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-05-29 18:55:23 +0900

    Fix to sqlite3 style

--- models/discarded_food.rb
+++ models/discarded_food.rb
@@ -4,9 +4,10 @@ class DiscardedFood < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   belongs_to :user
 
+  scope :where_my, -> (user_id) { where(user_id: user_id) }
   scope :where_current_year, -> { where(created_at: Time.current.all_year) }
   scope :where_current_month, -> { where(created_at: Time.current.all_month) }
-  scope :group_by_month, -> { group('MONTH(created_at)') }
-  scope :group_by_date, -> { group('DATE(created_at)') }
+  scope :group_by_month, -> { group('STRFTIME("%m", DATE(created_at))') }
+  scope :group_by_date, -> { group('STRFTIME("%d", DATE(created_at))') }
   scope :sum_calorie, -> { sum(:calorie) }
 end
