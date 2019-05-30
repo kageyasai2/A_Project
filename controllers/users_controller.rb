a39ab2bdb52d395b1c9c0a62a4ff9e36e00cf9ed
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-05-30 16:50:03 +0900

    Add signup error view

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -1,34 +1,23 @@
 require 'sinatra'
 require_relative 'base'
 
-require './models/user.rb'
-
 class UsersController < Base
   get '/' do
-    flash[:errors] = {}
     erb :'users/signup'
   end
 
   post '/' do
-    user = User.new({
+    @user = User.new({
       name: params[:name],
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation],
     })
-    flash[:errors] = {}
-    if params[:password_confirmation] && user.save
-      p user.errors.messages[:email]
-      p flash[:errors][:email]
-      erb :'sessions/login'
+
+    if params[:password_confirmation] && @user.save
+      redirect :'auth/login'
     else
-      flash[:errors] = user.errors.messages
-      p user.errors.messages
-      if flash[:errors][:email].present?
-        p flash[:errors][:email]
-      end
       erb :'users/signup'
     end
   end
-
 end
