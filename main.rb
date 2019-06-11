1dd4e18ad4c5116271d401770c71df28b54eb259
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-06-11 14:16:42 +0900

    Add ja validations

--- main.rb
+++ main.rb
@@ -2,8 +2,18 @@ require 'sinatra'
 require 'sinatra/base'
 require_relative 'config/environments'
 require 'active_record'
-ActiveRecord::Base.configurations = YAML.load_file(File.join(ROOT_DIR, 'config/database.yml'))
-ActiveRecord::Base.establish_connection(settings.environment)
+require 'i18n'
+require 'i18n/backend/fallbacks'
+
+configure do
+  ActiveRecord::Base.configurations = YAML.load_file(File.join(ROOT_DIR, 'config/database.yml'))
+  ActiveRecord::Base.establish_connection(settings.environment)
+
+  I18n.load_path = Dir[File.join(settings.root, 'config/locales', '*.yml')]
+  I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
+  I18n.enforce_available_locales = false
+  I18n.default_locale = :ja
+end
 
 Dir[CONTROLLERS_DIR + '/*.rb'].each do |controller|
   require controller
