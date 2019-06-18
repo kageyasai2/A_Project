5bb3a1c5f01b25f2ade9505998e9c4c6c1665676
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-06-18 13:31:34 +0900

    [fix]無駄コード削除

--- models/date_helper.rb
+++ models/date_helper.rb
@@ -10,34 +10,34 @@ module DateHelper
 
     end
 
-    def fill_calorie_as_total_kill_retio(kill_retios_hash)
-      hash_length = kill_retios_hash.length 
-      if hash_length < 2
-        return kill_retios_hash
+    def fill_calorie_as_total_kill_retio(kill_retios)
+      kill_retios_array = kill_retios.to_a
+      array_size = kill_retios_array.size
+      if array_size < 2
+        return kill_retios_array.to_h
       end
 
-      kill_retios_array = kill_retios_hash.to_a
-      hash_length.times do |i|
-        if i == hash_length - 1
+      array_size.times do |i|
+        if i == array_size - 1
           break
         end
 
         tmp_day1 = kill_retios_array[i][0].to_i
         tmp_day2 = kill_retios_array[i+1][0].to_i
         diff_day = tmp_day2 - tmp_day1
-        if diff_day < 1
+        if diff_day == 1
           next
         end
 
         kill_retio = kill_retios_array[i][1]
         (tmp_day1+1..tmp_day2-1).each do |index|
           day = '%02d' % (index)
-          kill_retios_hash.store(day,kill_retio)
+          kill_retios_array.push([day,kill_retio])
         end
 
       end
 
-      kill_retios_hash
+      kill_retios_array.to_h
     end
 
     def fill_calorie_of_monthly_as_zero(monthly_calories)
