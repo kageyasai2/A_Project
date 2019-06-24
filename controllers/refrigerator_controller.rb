6013eaf644a940554c6f88812f568adf8dbc2390
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-06-24 23:23:51 +0900

    Add refrigerator post controller

--- controllers/refrigerator_controller.rb
+++ controllers/refrigerator_controller.rb
@@ -10,6 +10,42 @@ class RefrigeratorController < Base
   end
 
   get '/' do
+    @user_foods = UserFood.where_my(@current_user.id)
     erb :'refrigerator/index'
+  end
+
+  post '/' do
+    @user_foods = UserFood.where_my(@current_user.id)
+    UserFood.transaction(joinable: false, requires_new: true) do
+      register_user_foods!(items: params[:items], user_id: session[:user_id])
+    rescue ActiveRecord::RecordInvalid
+      flash[:error] = '保存に失敗しました'
+      return erb :'/refrigerator/index'
+    end
+
+    redirect '/refrigerator'
+  end
+
+  private
+
+  def register_user_foods!(items:, user_id:)
+    raise ActiveRecord::RecordInvalid if items.nil?
+
+    is_err = false
+    @posted_foods =
+      items.map do |item|
+        user_food = UserFood.new({
+          name: item[:food_name],
+          limit_date: item[:date],
+          user_id: user_id,
+          gram: item[:gram],
+          calorie: rand(100),
+        })
+        is_err = true unless user_food.save
+
+        user_food
+      end
+
+    raise ActiveRecord::RecordInvalid if is_err
   end
 end
