185ce89fc1ac913e68a14df9e5e0cc9cee172f0f
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-06-26 15:27:38 +0900

    Fix access test

--- spec/requests/index_controller_spec.rb
+++ spec/requests/index_controller_spec.rb
@@ -1,8 +1,21 @@
 require 'spec_helper'
 
 describe IndexController, type: :request do
-  after(:all) do
+  before(:all) do
+    # テストするためにUserを全て削除する
+    User.destroy_all
     UserFood.delete_all
+
+    @user = User.create(
+      name: 'NAKKA',
+      email: 'nakka@example.com',
+      password: 'password',
+      password_confirmation: 'password',
+    )
+    UserFood.create(
+      name: '豚肉',
+      user_id: @user.id,
+    )
   end
 
   describe 'GET to /' do
@@ -13,32 +26,16 @@ describe IndexController, type: :request do
   end
 
   describe 'GET to /home' do
-    before(:all) do
-      # テストするためにUserを全て削除する
-      User.destroy_all
-
-      @user = User.create(
-        name: 'NAKKA',
-        email: 'nakka@example.com',
-        password: 'password',
-        password_confirmation: 'password',
-      )
-      UserFood.create(
-        name: '豚肉',
-        user_id: @user.id,
-      )
-    end
-
     it 'return own user_foods, when loggedin' do
       get '/home', {}, 'rack.session' => { user_id: @user.id }
       expect(last_response.status).to eq 200
-      expect(last_response.body).to include '豚肉'
+      expect(last_response.body).to include '今日のおすすめ'
     end
 
     it 'not return user_foods, when not loggein' do
       get '/home', {}, 'rack.session' => { user_id: nil }
-      expect(last_response.status).to eq 200
-      expect(last_response.body).not_to include '豚肉'
+      expect(last_response.status).to eq 302
+      expect(last_response.body).not_to include 'ログイン'
     end
   end
 end
